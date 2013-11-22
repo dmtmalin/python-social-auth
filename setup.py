0a486d796155e1a306c46a30e71b051b7ca874c6
zzzombat <zzzombat@gmail.com>
2013-11-22 17:51:42 +0400

    Add deps, so hackish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Setup file for easy installation"""
 import sys
 from os.path import join, dirname
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 PY3 = sys.version_info[0] == 3
@@ -28,12 +28,12 @@ def long_description():
         return LONG_DESCRIPTION
 
 
-requires = ['requests>=1.1.0', 'oauthlib>=0.3.8', 'six>=1.2.0']
-if PY3:
-    requires += ['python3-openid>=3.0.1',
-                 'requests-oauthlib>=0.3.0,<0.3.2']
-else:
-    requires += ['python-openid>=2.2', 'requests-oauthlib>=0.3.0']
+# requires = ['requests>=1.1.0', 'oauthlib>=0.3.8', 'six>=1.2.0']
+# if PY3:
+#     requires += ['python3-openid>=3.0.1',
+#                  'requests-oauthlib>=0.3.0,<0.3.2']
+# else:
+#     requires += ['python-openid>=2.2', 'requests-oauthlib>=0.3.0']
 
 setup(name='python-social-auth',
       version=version,
@@ -43,24 +43,33 @@ setup(name='python-social-auth',
       license='BSD',
       keywords='django, flask, pyramid, webpy, openid, oauth, social auth',
       url='https://github.com/omab/python-social-auth',
-      packages=['social',
-                'social.storage',
-                'social.apps',
-                'social.apps.django_app',
-                'social.apps.django_app.default',
-                'social.apps.django_app.me',
-                'social.apps.webpy_app',
-                'social.apps.flask_app',
-                'social.backends',
-                'social.pipeline',
-                'social.strategies',
-                'social.tests',
-                'social.tests.actions',
-                'social.tests.backends',
-            ],
+      py_modules=[
+          'six'
+      ],
+      packages=find_packages(),
+      # packages=[
+      #     'openid',
+      #     'requests',
+      #     'oauthlib',
+      #     'requests_oauthlib',
+      #     'social',
+      #     'social.storage',
+      #     'social.apps',
+      #     'social.apps.django_app',
+      #     'social.apps.django_app.default',
+      #     'social.apps.django_app.me',
+      #     'social.apps.webpy_app',
+      #     'social.apps.flask_app',
+      #     'social.backends',
+      #     'social.pipeline',
+      #     'social.strategies',
+      #     'social.tests',
+      #     'social.tests.actions',
+      #     'social.tests.backends',
+      #       ],
       #package_data={'social': ['locale/*/LC_MESSAGES/*']},
       long_description=long_description(),
-      install_requires=requires,
+      # install_requires=requires,
       classifiers=['Development Status :: 4 - Beta',
                    'Topic :: Internet',
                    'License :: OSI Approved :: BSD License',
