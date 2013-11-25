d09f4763ce12873b26dc318dac761e3df926147e
zzzombat <zzzombat@gmail.com>
2013-11-25 12:01:22 +0400

    Add debian dir

---
+++ debian/python-social-auth-full/usr/share/pyshared/social/__init__.py
@@ -0,0 +1,7 @@
+"""
+python-social-auth application, allows OpenId or OAuth user
+registration/authentication just adding a few configurations.
+"""
+version = (0, 1, 14)
+extra = '-hh2'
+__version__ = '.'.join(map(str, version)) + extra
---
+++ debian/python-social-auth-full/usr/share/pyshared/social/backends/hhru.py
@@ -0,0 +1,49 @@
+"""
+HH.ru OAuth2 support
+
+Take a look to https://github.com/hhru/api/blob/master/docs/authorization.md
+
+You need to register OAuth site here:
+https://dev.hh.ru/
+
+Then update your settings values using registration information
+
+"""
+
+import urllib2
+import json
+
+from social.backends.oauth import BaseOAuth2
+
+
+class HhruOAuth2(BaseOAuth2):
+    """HH.ru OAuth2 support"""
+    name = 'hhru-oauth2'
+    AUTHORIZATION_URL = 'https://m.hh.ru/oauth/authorize'
+    ACCESS_TOKEN_URL = 'https://m.hh.ru/oauth/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+    ID_KEY = 'id'
+
+    def auth_complete(self, *args, **kwargs):
+        try:
+            return super(HhruOAuth2, self).auth_complete(*args, **kwargs)
+        except urllib2.HTTPError:
+            raise Exception(self)
+
+    def get_user_details(self, response):
+        username = ''.join(('hhru_', response.get('email', '').split('@')[0], '_', str(response.get('id'))))
+        return {'username': username,
+                'email': response.get('email'),
+                'first_name': response.get('first_name'),
+                'last_name': response.get('last_name'),
+                'is_employer': response.get('is_employer'),
+                'employer': response.get('employer'),
+                }
+
+    def user_data(self, access_token, response, *args, **kwargs):
+        """Loads user data from service"""
+        request = urllib2.Request("http://api.hh.ru/me",
+                                  headers={"Authorization": "Bearer {token}".format(token=access_token)})
+        data = urllib2.urlopen(request).read()
+
+        return json.loads(data)
