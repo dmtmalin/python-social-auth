a70cdd004cbb51c871fd7f41278fcb348ab928ca
Nikolay <mikola1717@gmail.com>
2015-05-25 17:01:59 +0300

    Correct RemovedInDjango19Warning

--- social/__init__.py
+++ social/__init__.py
@@ -3,5 +3,5 @@ python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
 version = (0, 1, 22)
-extra = '-hh4'
+extra = '-hh5'
 __version__ = '.'.join(map(str, version)) + extra
